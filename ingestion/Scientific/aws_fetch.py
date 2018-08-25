c77a8518f09ce84279cf87ae7dfe851de8072706
Siddharth Saraf <xpress.siddharth@gmail.com>
2018-08-26 02:21:16 +0530

    Update aws_fetch.py

--- ingestion/Scientific/aws_fetch.py
+++ ingestion/Scientific/aws_fetch.py
@@ -3,10 +3,10 @@ client = boto3.client('s3')
 
 #Replace your bucket name and in prefix, file name 
 s3_bucket=""
-file_name=""
+file_path_with_name=""
 key=""
 
-resp = client.list_objects_v2(Bucket=s3_bucket, Prefix=file_name) 
+resp = client.list_objects_v2(Bucket=s3_bucket, Prefix=file_path_with_name) 
 
 for obj in resp['Contents']:
     key = obj['Key']
